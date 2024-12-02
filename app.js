f5d54da8473c344faebeafecfcf7bec7067f4b18
Yash Vishnoi <88367411+YashVishnoi47@users.noreply.github.com>
2024-12-02 12:17:08 +0530

    initialized index router

--- app.js
+++ app.js
@@ -3,18 +3,11 @@ const mongoose = require("mongoose");
 const path = require("path");
 const express = require("express");
 const flash = require("connect-flash");
-const connectDB = require('./src/config/db')
-const dbgr = require('debug')('app:app');
-const router = express.Router();
+const connectDB = require("./src/config/db");
+const dbgr = require("debug")("app:app");
 const app = express();
 
 connectDB();
-
-
-router.get('/',function(req,res){
-    res.send("hello")
-})
-
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
@@ -26,12 +19,14 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 
-const port = process.env.PORT || 3000
-app.listen(port,()=>{
-    if(process.env.NODE_ENV==='development'){
-        dbgr(`server is running on ${port}`);
-    }
+const indexRouter = require('./routes/index');
+app.use("/",indexRouter);
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  if (process.env.NODE_ENV === "development") {
+    dbgr(`server is running on ${port}`);
+  }
 });
-
-
-
